a6a63ec2989b35ee0ee4946f7068803bccba8790
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-11-08 21:09:23 +0700

    Menambahkan FormatUI.kt

--- app/src/main/java/com/example/activity5/data/SumberData.kt
+++ app/src/main/java/com/example/activity5/data/SumberData.kt
@@ -6,6 +6,8 @@ object SumberData {
     val flavors = listOf(
         R.string.original,
         R.string.vanilla,
-        
+        R.string.lemon,
+        R.string.milk,
+        R.string.lychee
     )
 }
