ce23d3abb9527a38ced6cb2dd2a677dea7998282
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-11-08 20:47:45 +0700

    Menambahkan foto ke drawable, menambah string, dimen, dan membuat object datasource(sumberdata)

---
+++ app/src/main/java/com/example/activity5/data/OrderUIState.kt
@@ -0,0 +1,3 @@
+package com.example.activity5.data
+
+data class OrderUIState()
---
+++ app/src/main/java/com/example/activity5/data/SumberData.kt
@@ -0,0 +1,11 @@
+package com.example.activity5.data
+
+import com.example.activity5.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        
+    )
+}
